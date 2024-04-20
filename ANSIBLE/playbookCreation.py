c8e9d50db80526e571fc980785fa6210b204ee20
Logan-Chayet <loch1722@colorado.edu>
2024-04-19 20:52:42 -0600

    minor error in testing

--- ANSIBLE/playbookCreation.py
+++ ANSIBLE/playbookCreation.py
@@ -19,7 +19,7 @@ def createVarPlaybook():
         interfaceNames = []
         loopback = ""
         processID = ""
-        with open('config_requirements.csv', newline='') as file:
+        with open('/home/student/Lab11PoCGithub/ANSIBLE/config_requirements.csv', newline='') as file:
             csvreader = csv.DictReader(file)
             for row in csvreader:
                 interface_name = row['Interface Name']
@@ -54,7 +54,7 @@ def createVarPlaybook():
                 'ospfNetwork': OSPFIPs,
                 'ospfArea': "0",
                 })
-    writeYaml(data, '/var/lib/jenkins/workspace/Lab11PoC/ANSIBLE/roles/router/vars/main')
+    writeYaml(data, '/home/student/Lab11PoCGithub/ANSIBLE/roles/router/vars/main')
 
 def getRouterIPs(routers):
     IPs = []
@@ -79,11 +79,11 @@ def sendConfigs():
             'password': 'password',
         }
         with ConnectHandler(**device) as connection:
-            with open("/var/lib/jenkins/workspace/Lab11PoC/ANSIBLE/CFGS/"+routers[i]+".txt", 'r') as file:
+            with open("/home/student/Lab11PoCGithub/ANSIBLE/CFGS/"+routers[i]+".txt", 'r') as file:
                 config_commands = file.readlines()
             output = connection.send_config_set(config_commands)
             print(output)
 
 createVarPlaybook()
-getCommand(["ansible-playbook", "site.yaml"])
+getCommand(["ansible-playbook", "/home/student/Lab11PoCGithub/ANSIBLE/site.yaml"])
 sendConfigs()
